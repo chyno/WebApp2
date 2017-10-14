e407fe601e63ffccf672f711581f3a091c0e1d46
jchynoweth <jchynowe1@yahoo.com>
2017-10-14 07:50:20 -0400

    some changes

---
+++ WebApp2tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApp2tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
